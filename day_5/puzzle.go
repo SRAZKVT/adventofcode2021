bd7af4d9ffd3d0f47bccaa91e7e46f89da8b88d1
Sarah-Mathey <sarah.mathey66666@gmail.com>
2021-12-05 15:41:42 +0100

    day 5 puzzle 2

--- day_5/puzzle.go
+++ day_5/puzzle.go
@@ -17,7 +17,28 @@ func main() {
 }
 
 func puzzle2() {
-	fmt.Println("puzzle 2:", "not yet implemented")
+	file, err := os.Open("day_5/input.txt")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	scanner := bufio.NewScanner(file)
+
+	var mapOfVents [SIZE][SIZE]int
+
+	for scanner.Scan() {
+		positions := strings.Split(scanner.Text(), " -> ")
+		mapOfVents = markPositionsHorizontalVertical(mapOfVents, positions)
+		mapOfVents = markPositionsDiagonal(mapOfVents, positions)
+	}
+	count := 0
+	for _, line := range mapOfVents {
+		for _, spot := range line {
+			if spot >= 2 {
+				count++
+			}
+		}
+	}
+	fmt.Println("puzzle 2:", count)
 }
 
 func puzzle1() {
@@ -64,6 +85,35 @@ func markPositionsHorizontalVertical(mapOfVents [SIZE][SIZE]int, positions []str
 	return mapOfVents
 }
 
+func markPositionsDiagonal(mapOfVents [SIZE][SIZE]int, positions []string) [SIZE][SIZE]int {
+	pos1 := getPosition(positions[0])
+	pos2 := getPosition(positions[1])
+
+	// straight line already done
+	if pos1[0] == pos2[0] || pos1[1] == pos2[1] {
+		return mapOfVents
+	}
+
+	// wait i'm dumb i can just iterate over x and just decrease or increse y, more complex is unnecessary
+	shouldBeReverse := (pos1[0] > pos2[0] || pos1[1] > pos2[1]) && !(pos1[0] > pos2[0] && pos1[1] > pos2[1]) // bruh why no XOR
+
+	var minPos, maxPos, startPos [2]int
+	minPos[0], maxPos[0] = min(pos1[0], pos2[0]), max(pos1[0], pos2[0])
+	minPos[1], maxPos[1] = min(pos1[1], pos2[1]), max(pos1[1], pos2[1])
+	startPos[0], startPos[1] = minPos[0], minPos[1]
+	if shouldBeReverse {
+		startPos[1] = maxPos[1]
+	}
+	for i := 0; i < maxPos[1]-(minPos[1]-1); i++ {
+		if shouldBeReverse {
+			mapOfVents[startPos[1]-i][startPos[0]+i]++
+		} else {
+			mapOfVents[startPos[1]+i][startPos[0]+i]++
+		}
+	}
+	return mapOfVents
+}
+
 func getPosition(s string) []int {
 	var result []int
 	values := strings.Split(s, ",")
@@ -76,4 +126,18 @@ func getPosition(s string) []int {
 
 func isWithinBounds(i, pos1, pos2 int) bool {
 	return (i >= pos2 && i <= pos1) || (i <= pos2 && i >= pos1)
+}
+
+func min(x1, x2 int) int {
+	if x1 < x2 {
+		return x1
+	}
+	return x2
+}
+
+func max(x1, x2 int) int {
+	if x1 > x2 {
+		return x1
+	}
+	return x2
 }
